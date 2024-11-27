a9cd2d48920332db01c81102754a5c0f236949cb
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 12:37:53 -0600

    Update outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -17,3 +17,18 @@ output "green_website_endpoint" {
   description = "The website endpoint for the Green environment"
   value       = module.s3_green.website_endpoint
 }
+
+output "blue_cloudfront_domain_name" {
+  description = "The domain name of the Blue CloudFront distribution"
+  value       = module.cloudfront_blue.cloudfront_domain_name
+}
+
+output "green_cloudfront_domain_name" {
+  description = "The domain name of the Green CloudFront distribution"
+  value       = module.cloudfront_green.cloudfront_domain_name
+}
+
+output "dns_record" {
+  description = "The DNS record created in Route 53"
+  value       = module.dns.dns_record
+}
