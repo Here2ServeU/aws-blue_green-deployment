a33a01b8ac019fe8ee0a56b9baa1a1a7aea79345
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 11:19:58 -0600

    Update main.tf

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -2,26 +2,33 @@ resource "aws_s3_bucket" "bucket" {
   bucket = "${var.name}-${terraform.workspace}"
   acl    = "private"
 
-  website {
-    index_document = "index.html"
+  tags = var.tags
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
   }
 
-  tags = var.tags
+  error_document {
+    key = "error.html"
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
   bucket = aws_s3_bucket.bucket.id
-  policy = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-    }
-  ]
-}
-EOT
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+      }
+    ]
+  })
 }
