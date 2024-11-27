c4b96a107c77d933f0661139adf255b8770531c4
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 12:17:47 -0600

    Update outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -3,17 +3,17 @@ output "blue_bucket_name" {
   value       = module.s3_blue.bucket_name
 }
 
+output "blue_website_endpoint" {
+  description = "The website endpoint for the Blue environment"
+  value       = module.s3_blue.website_endpoint
+}
+
 output "green_bucket_name" {
   description = "The name of the Green environment S3 bucket"
   value       = module.s3_green.bucket_name
 }
 
-output "cloudfront_domain_name" {
-  description = "The domain name of the CloudFront distribution"
-  value       = module.cloudfront.cloudfront_domain_name
-}
-
-output "dns_record" {
-  description = "The DNS record created for the CloudFront distribution"
-  value       = module.dns.dns_record
+output "green_website_endpoint" {
+  description = "The website endpoint for the Green environment"
+  value       = module.s3_green.website_endpoint
 }
