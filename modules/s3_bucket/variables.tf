2adcf9b361ea7df3ac8144b857151996d9df7bdc
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 09:43:58 -0600

    Create variables.tf

---
+++ modules/s3_bucket/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the bucket"
+  type        = map(string)
+  default     = {}
+}
