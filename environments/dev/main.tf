3ab236d4d9ddf6b8f39a697aa42897481dd3f178
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 12:36:50 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -17,3 +17,32 @@ module "s3_green" {
   }
   index_file  = "green-index.html"
 }
+
+module "cloudfront_blue" {
+  source                = "../../modules/cloudfront"
+  origin_domain_name    = module.s3_blue.website_endpoint
+  origin_id             = "S3-t2s-services-blue"
+  origin_access_identity = "origin-access-identity/cloudfront/E34T2SERVICESOAI"
+  tags                  = {
+    environment = var.environment
+    service     = "t2s-services"
+  }
+}
+
+module "cloudfront_green" {
+  source                = "../../modules/cloudfront"
+  origin_domain_name    = module.s3_green.website_endpoint
+  origin_id             = "S3-t2s-services-green"
+  origin_access_identity = "origin-access-identity/cloudfront/E34T2SERVICESOAI"
+  tags                  = {
+    environment = var.environment
+    service     = "t2s-services"
+  }
+}
+
+module "dns" {
+  source      = "../../modules/dns"
+  domain_name = "t2s-services.com"
+  record_name = "www"
+  target      = module.cloudfront_blue.cloudfront_domain_name
+}
