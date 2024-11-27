52cc786f6da1112e15faafcdbb044980290f07a4
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 14:58:14 -0600

    Update variables.tf

--- modules/cloudfront/variables.tf
+++ modules/cloudfront/variables.tf
@@ -1,25 +1,24 @@
 variable "origin_domain_name" {
-  description = "The domain name of the origin"
+  description = "The domain name of the origin (e.g., S3 bucket endpoint)"
   type        = string
 }
 
 variable "origin_id" {
-  description = "The ID of the origin"
+  description = "Unique ID for the origin"
   type        = string
 }
 
 variable "origin_access_identity" {
-  description = "The CloudFront Origin Access Identity"
+  description = "The CloudFront origin access identity"
+  type        = string
+}
+
+variable "logging_bucket" {
+  description = "S3 bucket for CloudFront access logs"
   type        = string
 }
 
 variable "tags" {
   description = "Tags for the CloudFront distribution"
   type        = map(string)
-  default     = {}
-}
-
-variable "logging_bucket" {
-  description = "S3 bucket for CloudFront access logs"
-  type        = string
 }
