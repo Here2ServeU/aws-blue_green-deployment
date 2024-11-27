0149a19269495508245e4fd66e5bfdfd8c4d23fe
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 12:47:54 -0600

    Update outputs.tf

--- modules/s3_bucket/outputs.tf
+++ modules/s3_bucket/outputs.tf
@@ -3,7 +3,11 @@ output "bucket_name" {
   value       = aws_s3_bucket.bucket.bucket
 }
 
+resource "aws_s3_bucket_website" "website_config" {
+  bucket = aws_s3_bucket.bucket.id
+}
+
 output "website_endpoint" {
   description = "The website endpoint for the S3 bucket"
-  value       = aws_s3_bucket.bucket.website_endpoint
+  value       = aws_s3_bucket_website.website_config.website_endpoint
 }
