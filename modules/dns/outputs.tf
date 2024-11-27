22d5d201d5408077931e63a58c32ad184a1301d7
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 12:33:34 -0600

    Update outputs.tf

--- modules/dns/outputs.tf
+++ modules/dns/outputs.tf
@@ -1,3 +1,9 @@
+output "zone_id" {
+  description = "The ID of the DNS zone"
+  value       = aws_route53_zone.zone.id
+}
+
 output "dns_record" {
-  value = aws_route53_record.record.fqdn
+  description = "The fully qualified domain name of the record"
+  value       = aws_route53_record.cname.fqdn
 }
