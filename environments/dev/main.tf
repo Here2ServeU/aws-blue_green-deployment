9c81889cca3fcf33241fe5883ead3521630dc2b7
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 13:54:55 -0600

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,3 +1,36 @@
+resource "aws_s3_bucket" "cloudfront_logs" {
+  bucket = "t2s-cloudfront-logs"
+  acl    = "private"
+
+  tags = {
+    environment = var.environment
+    service     = "t2s-services"
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudfront_logs_policy" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action    = "s3:PutObject"
+        Resource  = "arn:aws:s3:::t2s-cloudfront-logs/*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/*"
+          }
+        }
+      }
+    ]
+  })
+}
+
 module "s3_blue" {
   source      = "../../modules/s3_bucket"
   name        = "t2s-services-blue"
@@ -35,6 +68,7 @@ module "cloudfront_green" {
   origin_domain_name    = module.s3_green.website_endpoint
   origin_id             = "S3-t2s-services-green"
   origin_access_identity = "origin-access-identity/cloudfront/E34T2SERVICESOAI"
+  logging_bucket        = "t2s-cloudfront-logs"
   tags                  = {
     environment = var.environment
     service     = "t2s-services"
