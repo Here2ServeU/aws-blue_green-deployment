0ddd267c38f1257ac3e67c33847260b88ccb817b
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 09:53:50 -0600

    Create main.tf

---
+++ environments/stage/main.tf
@@ -0,0 +1,35 @@
+module "s3_blue" {
+  source = "../../modules/s3_bucket"
+  name   = "t2s-services-blue"
+  tags   = {
+    environment = var.environment
+    service     = "t2s-services"
+  }
+}
+
+module "s3_green" {
+  source = "../../modules/s3_bucket"
+  name   = "t2s-services-green"
+  tags   = {
+    environment = var.environment
+    service     = "t2s-services"
+  }
+}
+
+module "cloudfront" {
+  source                = "../../modules/cloudfront"
+  origin_domain_name    = module.s3_blue.bucket_name
+  origin_id             = "S3-t2s-services-blue"
+  origin_access_identity = "origin-access-identity/cloudfront/E34EXAMPLEOAI"
+  tags                  = {
+    environment = var.environment
+    service     = "t2s-services"
+  }
+}
+
+module "dns" {
+  source      = "../../modules/dns"
+  zone_id     = var.zone_id
+  record_name = "www.t2s-services.com"
+  target      = module.cloudfront.cloudfront_domain_name
+}
