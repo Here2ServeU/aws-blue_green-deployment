f4b3e4113a9de0c9f39439856f986e83219bc262
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 13:51:47 -0600

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "environment" {
-  description = "Deployment environment (e.g., dev, stage, prod)"
-  type        = string
-}
-
-variable "zone_id" {
-  description = "Route 53 Zone ID"
-  type        = string
-}
