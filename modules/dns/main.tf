ab755e55e2f67c45600751b7128f07ab1669c58d
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 12:30:14 -0600

    Update main.tf

--- modules/dns/main.tf
+++ modules/dns/main.tf
@@ -1,5 +1,9 @@
-resource "aws_route53_record" "record" {
-  zone_id = var.zone_id
+resource "aws_route53_zone" "zone" {
+  name = var.domain_name
+}
+
+resource "aws_route53_record" "cname" {
+  zone_id = aws_route53_zone.zone.id
   name    = var.record_name
   type    = "CNAME"
   ttl     = 300
