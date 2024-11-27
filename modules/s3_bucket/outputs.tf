6506a491c732c5bdca232d203180b836e7545c8a
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 15:32:16 -0600

    Update outputs.tf

--- modules/s3_bucket/outputs.tf
+++ modules/s3_bucket/outputs.tf
@@ -1,6 +1,6 @@
 output "bucket_name" {
   description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.bucket.bucket
+  value       = aws_s3_bucket.bucket.id
 }
 
 output "website_endpoint" {
