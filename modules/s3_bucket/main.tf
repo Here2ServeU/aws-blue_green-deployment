992ad2ce5426d76018d731132a9f3ae1be0165be
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 11:54:23 -0600

    Update main.tf

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,41 +1,17 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.name}-${terraform.workspace}"
-  acl    = "private"
-
-  tags = var.tags
-}
-
-resource "aws_s3_bucket_website_configuration" "website" {
-  bucket = aws_s3_bucket.bucket.id
-
-  index_document {
-    suffix = "index.html"
+module "s3_blue" {
+  source      = "../../modules/s3_bucket"
+  name        = "t2s-services-blue"
+  tags        = {
+    environment = var.environment
   }
+  index_file  = var.index_file
+}
 
-  error_document {
-    key = "error.html"
+module "s3_green" {
+  source      = "../../modules/s3_bucket"
+  name        = "t2s-services-green"
+  tags        = {
+    environment = var.environment
   }
-}
-
-resource "aws_s3_bucket_object" "index_file" {
-  bucket = aws_s3_bucket.bucket.id
-  key    = "index.html"
-  source = var.index_file
-  content_type = "text/html"
-}
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
-      }
-    ]
-  })
+  index_file  = var.index_file
 }
