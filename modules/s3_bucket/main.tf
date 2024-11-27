3abe45a2a2d53c3d19abd6b8a641df9a0923420a
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-11-27 11:59:41 -0600

    Update main.tf

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -22,3 +22,19 @@ resource "aws_s3_bucket_object" "index_file" {
   source       = var.index_file
   content_type = "text/html"
 }
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.bucket.id}/*"
+      }
+    ]
+  })
+}
